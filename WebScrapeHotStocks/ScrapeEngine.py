b84f83ce5af3bb7ee8f64896d1cc1da45fea1c4f
hugocoussens <hbcoussens@gmail.com>
2022-08-19 17:27:23 +0100

    refactor

---
+++ WebScrapeHotStocks/ScrapeEngine.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+from bs4 import Comment
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from datetime import datetime
+import mysql.connector
