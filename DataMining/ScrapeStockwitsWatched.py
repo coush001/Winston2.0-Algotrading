460a124d50cb7a8991a5afca9ef01403a2c22dd6
hugocoussens <hbcoussens@gmail.com>
2022-08-20 12:05:25 +0100

    add requirements.txt rename folders remove sql connection add data as csv folder rename paths

--- DataMining/ScrapeStockwitsWatched.py
+++ DataMining/ScrapeStockwitsWatched.py
@@ -1,9 +1,10 @@
+#!/bin/bash
+import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
-import mysql.connector
-
+import csv
 
 # function that will return soup from a url
 def get_soup(url="https://stocktwits.com/rankings/watchers"):
@@ -12,7 +13,6 @@ def get_soup(url="https://stocktwits.com/rankings/watchers"):
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     return soup
-
 
 def get_data_list(soup):
     # get All of the table row elements
@@ -23,7 +23,8 @@ def get_data_list(soup):
         ticker = i.find('span', class_='st_3ua_jwB st_8u0ePN3 st_1SuHTwr')  # get ticker by class
         count = i.find_all('td')  # get count of watches as first td tag
         price_move = i.find_all('span')[-1]  # get price move as the last span
-        sign=0
+        sign = 0
+
         if len(price_move['class']) < 4:
             sign = 0
         elif price_move['class'][3] == 'st_3ftxMPa':  # identify if pos or neg based on class
@@ -38,49 +39,19 @@ def get_data_list(soup):
     return output_data
 
 
-def get_cursor():
-    mydb = mysql.connector.connect(
-        host="35.246.42.55",
-        user="root",
-        password="RF/o<_4eyHm`>&^e"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("use stocks")
-    return mycursor, mydb
+def insert_rows(data):
+    field_names = ['datetime', 'ticker', 'watchers', 'pricemove']
 
-
-def select_all(mycursor, mydb):
-    mycursor.execute("use stocks")
-    mycursor.execute("show tables")
-    print(mycursor.fetchall())
-    mycursor.execute("select * from stocknote")
-    print(mycursor.fetchall())
-    print('shown all rows')
-
-
-def drop_all(mycursor, mydb):
-    mycursor.execute("use stocks")
-    mycursor.execute("delete from stocknote")
-    mydb.commit()
-    print('deleted all rows')
-
-
-def insert_rows(data, cursor, connection):
     for i in data:
-        mySql_insert_query = """INSERT INTO stocknote (datetime,ticker,watchers,pricechange) 
-                            VALUES (%s, %s, %s, %s) """
-
-        record = (i[0], i[1], i[2], i[3])
-        cursor.execute(mySql_insert_query, record)
-        connection.commit()
-        print("Record inserted successfully into stocknote table")
+        path = "/Users/hugocoussens/git/Winston2.0-Algotrading/DataMining/data/stockwitswatched.csv"
+        dict = {"datetime": i[0], "ticker": i[1], "watchers": i[2], "pricemove": i[3], }
+        with open(path, 'a') as csv_file:
+            dict_object = csv.DictWriter(csv_file, fieldnames=field_names)
+            dict_object.writerow(dict)
+    print("Record inserted successfully into stocknote table")
 
 if __name__ == "__main__":
     soup = get_soup()
     data = get_data_list(soup)
-    cursor, db = get_cursor()
-    insert_rows(data, cursor, db)
+    insert_rows(data)
     print("Successful upload of hot stocks data to sql data base  ", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-    # select_all(cursor, db)
-    # # drop_all(cursor, db)
-
